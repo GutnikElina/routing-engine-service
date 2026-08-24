22a29bba2ea8211a4d0e35d0b5da1c656e3ec221
zhenya <zenamatusev68@gmail.com>
2026-08-24 20:44:30 +0300

    feat(route): integrate OpenAPI generator and refactor RouteController to use generated models

--- src/main/java/com/logistics/routing/adapter/in/web/RouteController.java
+++ src/main/java/com/logistics/routing/adapter/in/web/RouteController.java
@@ -1,32 +1,25 @@
 package com.logistics.routing.adapter.in.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.http.ResponseEntity;     
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import com.logistics.routing.adapter.in.web.dto.request.CreateRouteRequest;
-import com.logistics.routing.adapter.in.web.dto.response.CreateRouteResponse;
-import com.logistics.routing.application.route.create.CreateRouteUseCase;
+import com.logistics.routing.adapter.in.web.generated.api.RoutesApi;
+import com.logistics.routing.adapter.in.web.generated.model.CreateRouteRequest;
+import com.logistics.routing.adapter.in.web.generated.model.CreateRouteResponse;
 import com.logistics.routing.adapter.in.web.mapper.RouteWebMapper;
-import jakarta.validation.Valid;
-import com.logistics.routing.application.route.create.CreateRouteCommand;
-import com.logistics.routing.application.route.create.CreateRouteResult;
+import com.logistics.routing.application.route.create.CreateRouteUseCase;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/routes")
 @RequiredArgsConstructor
-public class RouteController {
+public class RouteController implements RoutesApi {
     private final CreateRouteUseCase createRouteUseCase;
     private final RouteWebMapper routeWebMapper;
-    @PostMapping
-    public ResponseEntity<CreateRouteResponse> createRoute(@Valid @RequestBody CreateRouteRequest request) {
-        CreateRouteCommand command = routeWebMapper.toCommand(request);
-        CreateRouteResult result = createRouteUseCase.execute(command);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(routeWebMapper.toResponse(result));
+    @Override
+    public ResponseEntity<CreateRouteResponse> createRoute(CreateRouteRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(routeWebMapper.toResponse(createRouteUseCase.execute(routeWebMapper.toCommand(request))));
     }
 }
